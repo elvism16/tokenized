30d3ab7eebd44e3bab4b99cf422c687d235d7a85
Marc <elvis.m16@gmail.com>
2023-04-05 15:31:37 +0200

    bugfix

--- pages/index_original.js
+++ pages/index_original.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Notification from "../components/notification/notification";
 import Section from "../components/section/section";
 
